a82a6b66c5bcb00cb19ff567277ac0121e933380
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-04-24 10:39:09 +0200

    image geroteerd

--- src/view/Tile.java
+++ src/view/Tile.java
@@ -8,6 +8,7 @@ package view;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.geom.AffineTransform;
 import javax.swing.ImageIcon;
 
 /**
@@ -23,8 +24,17 @@ public class Tile {
 	this.pos = pos;
 	this.tile = new ImageIcon(path).getImage();
     }
+    
+    private AffineTransform setTransform(){
+	AffineTransform transformer = new AffineTransform();
+	transformer.translate(pos.getX(), pos.getY());
+	transformer.scale(1,1);
+	transformer.rotate(Math.toRadians(-45), tile.getWidth(null)/2, tile.getHeight(null)/2);
+	return transformer;
+    }
 
     public void draw(Graphics2D g2) {
-	g2.drawImage(tile, pos.x, pos.y, null);
+	AffineTransform tx = setTransform();
+	g2.drawImage(tile, tx, null);
     }
 }
